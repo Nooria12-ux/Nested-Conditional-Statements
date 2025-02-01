3734bf40c4e7d81b4bfb6f38a53a2a35ad85c359
Nooria <samihamaysoonnooria@gmail.com>
2025-02-01 21:19:22 +0600

    ghdgfbfg

---
+++ Check age.py
@@ -0,0 +1,9 @@
+age = int(input("How old are you?: "))
+
+if age > 9:
+    if age>=10 and age <= 20:
+        print("You are allowed in the class, age limit in between 10 and 20. Congrats")
+    else:
+        print("You are not allowed bc you are diddy. Well sad life")
+else:
+    print("You are too young for the class. Go eat fider") 
